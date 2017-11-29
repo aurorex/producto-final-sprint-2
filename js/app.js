871c071c2ae6c73a358b0306f78bdf25d4165bfd
aurora <selva_beat@hotmail.com>
2017-11-29 18:27:45 -0500

    agregando funcionalidad y header

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
 window.addEventListener('load', function() {
-  var input = document.getElementById('input');
+  var anchorStudents = document.getElementById('anchor-students');
   console.log(data);
-  input.addEventListener('click', function(event) {
+  anchorStudents.addEventListener('click', function(event) {
     var cumulative = '';
-    var nameX = document.createElement('h1');
-    var addNames = document.getElementById('add-Names');
-
+   
     for (i = 0; i < data.AQP['2016-2'].students.length;i++) {
       // var name = document.getElementById('name');
       // name.textContent = data.AQP['2016-2'].students[i].name;
+      var nameX = document.createElement('h1');
+      var addNames = document.getElementById('add-Names');
+      nameX.setAttribute('class', 'students-skills');
       console.log(data.AQP['2016-2'].students[i].name);
       console.log(cumulative += data.AQP['2016-2'].students[i].name);
       nameX.textContent = data.AQP['2016-2'].students[i].name;
       console.log(nameX);
       addNames.appendChild(nameX);
+      var secondNamex = addNames.getElementsByTagName('h1')[0];
+      addNames.insertBefore(nameX, secondNamex);
+      event.preventDefault();
     }
     /* var score1 = document.getElementById('score-1');
     for (i = 0; i < data.AQP['2016-2'].students[i].sprints[i].score['tech'].length;i++) {
@@ -24,4 +28,5 @@ window.addEventListener('load', function() {
     var score2 = document.getElementById('score-2');
     score2.textContent = data.AQP['2016-2'].students[0].sprints[0].score['hse'];*/
   });
+  
 });
